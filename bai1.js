0d7b021a4859a829ec0ec5862e90069f8380591a
rosaduong <tantai@tantais-MacBook-Air.local>
2024-04-14 21:11:21 +0700

    xong bai 1

---
+++ bai1.js
@@ -0,0 +1,4 @@
+document.getElementById("name").innerHTML = " Nguyen Thai Thuy Duong "
+document.getElementById("age").innerHTML =" 17 "
+document.getElementById("school").innerHTML = " An Lac High school "
+document.getElementById("favourite").innerHTML = " xem phim "
