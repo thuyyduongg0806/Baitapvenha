da8d8a204ad80dc6ae4788cd27dbd3989247e468
rosaduong <tantai@tantais-MacBook-Air.local>
2024-04-14 21:49:45 +0700

    xg   bai     2 3 4

---
+++ bai3.js
@@ -0,0 +1,12 @@
+let a = 23
+let b = 30
+if (a>b){
+    console.log("a lon hon ")
+
+}
+else if (b>a){
+    console.log("b lon hon a")
+}
+else {
+    console.log("hai so bang nhau")
+}
---
+++ bai4.js
@@ -0,0 +1,4 @@
+let diemtb = 3
+if (diemtb >= 8.5 && diemtb <= 10){
+    console.log("xep loai gioi")
+}
