3d6ab62f5184028a608fcd1ca018a1031537cf39
rosaduong <tantai@tantais-MacBook-Air.local>
2024-04-15 19:46:17 +0700

    xong bai 2

--- bai4.js
+++ bai4.js
@@ -2,3 +2,12 @@ let diemtb = 3
 if (diemtb >= 8.5 && diemtb <= 10){
     console.log("xep loai gioi")
 }
+else if ( diemtb >= 6.5 && diemtb < 8.5){
+    console.log("xep loai kha")
+}
+else if ( diemtb >= 5 && diemtb <6.5){
+    console.log("xep loai trung binh")
+}
+else {
+    console.log ("xep loai yeu")
+}
